40b6183b8dc0f55061008483f75a63a16b383876
blyxix <justin.takeda@gmail.com>
2019-04-15 11:33:44 +1200

    finish person details

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var userID; //global will use later
 var key = 'ZZI3LTKqP5pdKUyFTQkzuObtnAHxf5Ao';	
+//backup key - ZZI3LTKqP5pdKUyFTQkzuObtnAHxf5Ao 9AwInosQykaf21iKK6bM2H24oL5BJ1Wa
 var designerList = 'justintakeda'; 				
 var urlFollowing = 'https://api.behance.net/v2/users/' + designerList + '/following?client_id=' + key;
 
@@ -8,6 +9,9 @@ Handlebars.registerHelper("removeSpace",function(myClass){
 		var withLower = noSpace.charAt(0).toLowerCase() + noSpace.substring(1);
 		return withLower;
 });
+
+
+
 
 if($('#index').length > 0) {
 
@@ -20,6 +24,7 @@ if($('#index').length > 0) {
 			url: urlFollowing,
 				dataType: 'jsonp',
 				success: function(list) {
+
 					createHTML(list);
 					
 					function createHTML(data){
@@ -81,16 +86,19 @@ if($('#index').length > 0) {
 	});
 }//end home
 
+
+
 if($('#person').length > 0) {
 	var pageURL = new URL(document.location);
 	var params = pageURL.searchParams;
 	var behanceUser  = params.get('id');
-	var urlPerson = 'https://api.behance.net/v2/users/' + behanceUser + '/projects?client_id=' + key;
-	console.log(urlPerson);
+	var urlPersonProject = 'https://api.behance.net/v2/users/' + behanceUser + '/projects?client_id=' + key;
+	var urlPersonDetails = 'https://api.behance.net/v2/users/' + behanceUser + '?client_id=' + key;
+	
 
 	$(function(){
 		$.ajax({
-			url: urlPerson,
+			url: urlPersonProject,
 				dataType: 'jsonp',
 				success: function(list) {
 					createHTML(list);
@@ -98,10 +106,10 @@ if($('#person').length > 0) {
 					function createHTML(data){
 						/*Handlebars functionality*/
 						console.log(data);
-						var rawTemplate = document.getElementById("personDetails").innerHTML;
+						var rawTemplate = document.getElementById("projects").innerHTML;
 						var compiledTemplate = Handlebars.compile(rawTemplate);
 						var ourGeneratedHTML = compiledTemplate(data);
-						var designerContainer = document.getElementById("designerPerson");
+						var designerContainer = document.getElementById("displayProjects");
 						designerContainer.innerHTML = ourGeneratedHTML;
 						
 
@@ -111,7 +119,30 @@ if($('#person').length > 0) {
 
 					}// end function
 				}//end success
-		});
+		});//end ajax
+
+		$.ajax({
+			url: urlPersonDetails,
+				dataType: 'jsonp',
+				success: function(list) {
+					createHTML(list);
+					
+					function createHTML(data){
+					/*Handlebars functionality*/
+					console.log(data);
+					var rawTemplate = document.getElementById("personDetails").innerHTML;
+					var compiledTemplate = Handlebars.compile(rawTemplate);
+					var ourGeneratedHTML = compiledTemplate(data);
+					var designerContainer = document.getElementById("displayPerson");
+					designerContainer.innerHTML = ourGeneratedHTML;	
+
+
+						
+
+
+					}// end function
+				}//end success
+		});//end ajax
 	});
 }
 
