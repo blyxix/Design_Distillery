afdecdbdcab28e25982d6255d60f4fe4b2af694b
Justin keith tirado Takeda <845871329@cit3-501m-18.local>
2019-04-16 15:21:51 +1200

    added graphs

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 var userID; //global will use later
-var key = 'ZZI3LTKqP5pdKUyFTQkzuObtnAHxf5Ao';	
+var key = '9AwInosQykaf21iKK6bM2H24oL5BJ1Wa';	
 //backup key - ZZI3LTKqP5pdKUyFTQkzuObtnAHxf5Ao 9AwInosQykaf21iKK6bM2H24oL5BJ1Wa
 var designerList = 'justintakeda'; 				
 var urlFollowing = 'https://api.behance.net/v2/users/' + designerList + '/following?client_id=' + key;
+var userStats = [];
+
+
+
 
 Handlebars.registerHelper("removeSpace",function(myClass){
 		var noSpace = myClass.replace(/\s/g,'');
@@ -19,6 +23,34 @@ Handlebars.registerHelper('if_eq', function(a, b, opts) {
         return opts.inverse(this);
     }
 });
+
+Handlebars.registerHelper("grabData", function(data) {
+	userStats.push(data);
+	return "";
+});
+
+function grabAvg(data) {
+	avgViews.push(data.views);
+	avgApp.push(data.appreciations);
+	avgFollow.push(data.followers);
+	avgFollowing.push(data.following);
+
+};
+
+function calcAvg(persons){
+	var container = 0;
+	var avg = 0;
+	var counter = persons.length;
+	for(var i=0; i < persons.length; i++){
+		container += persons[i];
+	}
+	avg = container/counter;
+	avg = avg.toFixed(0);
+	console.log(avg);
+	return avg;
+}
+
+
 
 if($('#index').length > 0) {
 
@@ -101,9 +133,31 @@ if($('#person').length > 0) {
 	var behanceUser  = params.get('id');
 	var urlPersonProject = 'https://api.behance.net/v2/users/' + behanceUser + '/projects?client_id=' + key;
 	var urlPersonDetails = 'https://api.behance.net/v2/users/' + behanceUser + '?client_id=' + key;
+	var iAvg = 0;
+	var avgViews = [];
+	var avgApp = [];
+	var avgFollow = [];
+	var avgFollowing = [];
 	
 
 	$(function(){
+		$.ajax({
+			url: urlFollowing,
+			dataType: 'jsonp',
+			success: function(list) {
+				var count = Object.keys(list.following).length;
+
+
+				 for(i=0; i < count; i++){
+				 	grabAvg(list.following[i].stats);
+				 }
+
+				personInfo();
+
+			}//end success
+		});//end ajax
+
+
 		$.ajax({
 			url: urlPersonProject,
 				dataType: 'jsonp',
@@ -128,28 +182,88 @@ if($('#person').length > 0) {
 				}//end success
 		});//end ajax
 
-		$.ajax({
-			url: urlPersonDetails,
-				dataType: 'jsonp',
-				success: function(list) {
-					createHTML(list);
-					
-					function createHTML(data){
-					/*Handlebars functionality*/
-					console.log(data);
-					var rawTemplate = document.getElementById("personDetails").innerHTML;
-					var compiledTemplate = Handlebars.compile(rawTemplate);
-					var ourGeneratedHTML = compiledTemplate(data);
-					var designerContainer = document.getElementById("displayPerson");
-					designerContainer.innerHTML = ourGeneratedHTML;	
+		var personInfo = function(){
 
-
+			$.ajax({
+				url: urlPersonDetails,
+					dataType: 'jsonp',
+					success: function(list) {
+						createHTML(list);
+						
+						function createHTML(data){
+						/*Handlebars functionality*/
+						console.log(data);
+						var rawTemplate = document.getElementById("personDetails").innerHTML;
+						var compiledTemplate = Handlebars.compile(rawTemplate);
+						var ourGeneratedHTML = compiledTemplate(data);
+						var designerContainer = document.getElementById("displayPerson");
+						designerContainer.innerHTML = ourGeneratedHTML;	
 						
 
 
-					}// end function
-				}//end success
-		});//end ajax
+						var ctx = document.getElementById('myChart');
+						var avgViewsNo = calcAvg(avgViews);
+						var avgAppNo = calcAvg(avgApp);
+						var avgFollowNo = calcAvg(avgFollow);
+						var avgFollowingNo = calcAvg(avgFollowing);
+						var chartMax = 0; 
+						if(userStats[0] < 1000){
+							chartMax = userStats[0] + 100;
+						}else{
+							chartMax = userStats[0] + 1000;
+						}
+
+						
+						
+						var myChart = new Chart(ctx, {
+						    type: 'bar',
+						    data: {
+						        labels: ['Project views', 'Appreciations', 'Followers', 'Following'],
+						        datasets: [{
+						            label: 'Current Designer',
+						            data: userStats,
+						            backgroundColor: [
+						                'rgba(217, 134, 72, 0.5)',
+						                'rgba(217, 134, 72, 0.5)',
+						                'rgba(217, 134, 72, 0.5)',
+						                'rgba(217, 134, 72, 0.5)',
+						                'rgba(217, 134, 72, 0.5)',
+						                'rgba(217, 134, 72, 0.5)'
+						            ],
+						            borderWidth: 0
+						        },{
+						        	label: 'Agency Average',
+						        	data: [avgViewsNo, avgAppNo, avgFollowNo, avgFollowingNo],
+						            backgroundColor: [
+						                'rgba(52, 58, 64, 0.5)',
+						                'rgba(52, 58, 64, 0.5)',
+						                'rgba(52, 58, 64, 0.5)',
+						                'rgba(52, 58, 64, 0.5)',
+						                'rgba(52, 58, 64, 0.5)',
+						                'rgba(52, 58, 64, 0.5)'
+						            ],
+						            borderWidth: 0
+						        }]
+						    },
+						    options: {
+						        scales: {
+						            yAxes: [{
+						                ticks: {
+						                	max: chartMax,
+						                    beginAtZero: true,
+						                }
+						            }]
+						        }
+						    },
+						});
+
+							
+
+
+						}// end function
+					}//end success
+			});//end ajax
+		}
 	});
 }
 
